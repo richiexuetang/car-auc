059fb7bd4b170a615ee8e2ead06b6414111abae2
richiexue <richard772794659@gmail.com>
2024-10-10 03:41:19 -0700

    bids service

---
+++ src/BiddingService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BiddingService.DTOs;
+using BiddingService.Models;
+using CarAuc.BuildingBlocks.Contracts.EventBus.Messages;
+
+namespace BiddingService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Bid, BidDto>();
+        CreateMap<Bid, BidPlaced>();
+    }
+}
